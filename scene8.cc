8aa06946ec65b83b391a08fb2e57018907ccadcc
Paolo Bonzini <bonzini@gnu.org>
2005-03-09 14:02:01 +0000

    add another cute refraction scene
    2005-02-09  Paolo Bonzini  <bonzini@gnu.org>
    
    	* scene3.cc: Tweak specular component of the glass.
    	* scene8.cc: New file.
    	* Makefile.am: Add it.
    
    
    git-archimport-id: bonzini@gnu.org--2004b/ray--devo--1.0--patch-12

---
+++ scene8.cc
@@ -0,0 +1,31 @@
+// Example scene
+// Written by Paolo Bonzini, 2005
+
+#include "images.h"
+#include "scene.h"
+
+int main (void)
+{
+  Light l (10, 10, -30);
+  // Material floor (0, 0.8, 0, 0.1);
+  Material floor (0, 0.8, 0, 0);
+  Material glass (0.05, 0.2, 0.7, 0.2, 0.9, 1.33, 0.8);
+
+  Plane p (0, 1, 0, 0);
+  Sphere s (0, 5, -6, 5);
+
+  CheckerTexture floor_texture (colors::red, colors::white, 3);
+
+  Scene scene;
+  scene.add_light (l);
+  scene.add_object (p, floor, floor_texture);
+  scene.add_object (s, glass, MonoTexture::white);
+
+  Image image;
+  image.set_size (320, 240);
+
+  Point3D location (0, 5, -20);
+  Vector3D direction (0, 0, 1);
+  scene.render (location, direction, image);
+  image.write ("scene8.png", OUT_PNG);
+}
